37fa942110acfad85f56a8fe50b0254e995b439f
Srishti-04 <srishtijaiswal2823@gmail.com>
2026-04-21 19:55:33 +0530

    Fixed CI issue with headless browser

--- conftest.py
+++ conftest.py
@@ -1,15 +1,16 @@
-import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
 
 @pytest.fixture
 def driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
