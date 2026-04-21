cfd38cf82f50039e3715d0dc29829a897d33d9eb
Srishti-04 <srishtijaiswal2823@gmail.com>
2026-04-21 19:59:43 +0530

    Fixed CI by adding Chrome and Chromedriver

--- conftest.py
+++ conftest.py
@@ -1,13 +1,11 @@
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="chrome")
 
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")  # 👈 use this
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
