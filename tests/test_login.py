15f1c75277e03b729b570dab2e14632cc847b864
Srishti-04 <srishtijaiswal2823@gmail.com>
2026-04-21 19:47:06 +0530

    Upgraded framework with data-driven testing and reporting

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,10 +1,25 @@
+import pytest
+import json
 from pages.login_page import LoginPage
+from utils.logger import get_logger
+logger = get_logger()
 
-def test_valid_login(driver):
+with open("testdata.json") as f:
+    test_data = json.load(f)
+
+@pytest.mark.parametrize("username,password", [
+    (d["username"], d["password"]) for d in test_data
+])
+def test_login(driver, username, password):
+    logger.info("Starting login test")
+
     page = LoginPage(driver)
-
     page.open()
-    page.login("tomsmith", "SuperSecretPassword!")
+    page.login(username, password)
 
     message = page.get_message()
-    assert "You logged into a secure area!" in message
+
+    if username == "tomsmith":
+        assert "secure area" in message.lower()
+    else:
+        assert "invalid" in message.lower()
